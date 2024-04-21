2731fc8ca5361fbcfaa76153dadac6b1e4324963
Dennis Engelbert <dennis@theefamily.com>
2024-04-21 12:09:38 -0500

    Initial Commit

---
+++ G_Monitor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  G_Monitor
+//
+//  Created by Theefamily on 4/21/24.
+//  Copyright © 2024 Theefamily. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
